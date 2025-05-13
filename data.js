6376ff1884151ee07070047cf6d3a8793d2eb0cc
ekarik <ekarikapondo@gmail.com>
2025-05-13 12:12:41 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,493 @@
+klynt.data = {
+    "miniPlayer": {
+        "resumePlaybackWording": "Resume playback?",
+        "redirectToMobileApp": "auto",
+        "noWording": "No",
+        "analyticsKey": "",
+        "launchAppWording": "Then Launch Project",
+        "title": "TEST",
+        "yesWording": "Yes",
+        "downloadAppWording": "Download App",
+        "thanksForWatchingWording": "Thanks for watching!",
+        "fullscreenInfoWording": "This program will launch in fullscreen",
+        "url": "",
+        "description": "",
+        "thumbnail": "Medias/Photos/IMG_2393.jpeg"
+    },
+    "menu": {
+        "offset_sequence": 12,
+        "backgroundImage": null,
+        "searchWidget": "D79B1B39-58B7-12EB-FBD6-7CC758155F8A",
+        "displayBackgroundImage": false,
+        "widgets": [
+            {
+                "id": "E01AC54F-0FCA-A95E-1B15-7CC75816804F",
+                "type": "index",
+                "params": {
+                    "displayDescriptions": true,
+                    "layout": "grid",
+                    "sequences": [
+                        "0AB7F235-2012-2206-6968-7CC7582C3BF4",
+                        "0C7A1342-B5C6-03CD-3278-7CC9FB61D9AA",
+                        "27D0F491-3F41-F243-3294-7CCCD08F6FF2"
+                    ],
+                    "displayDuration": true
+                },
+                "label": "Index"
+            },
+            {
+                "id": "4AD34FA5-BF3D-59A4-DFBF-7CC7581484E2",
+                "type": "credits",
+                "params": {
+                    "subtitle": "",
+                    "aboutColumnText": "",
+                    "creditsColumnTitle": "Credits",
+                    "title": "TEST",
+                    "creditsColumnText": "",
+                    "aboutColumnTitle": "About"
+                },
+                "label": "Credits"
+            },
+            {
+                "resultsWording": "Results for: ",
+                "resultWording": "Result for: ",
+                "placeHolderWording": "Type here...",
+                "id": "D79B1B39-58B7-12EB-FBD6-7CC758155F8A",
+                "type": "search",
+                "enabled": true,
+                "label": "Search"
+            },
+            {
+                "displayDescriptions": false,
+                "smallSequenceSize": 25,
+                "displayWatermark": false,
+                "id": "5960A1F5-DAE0-B116-834C-7CC75815B3B4",
+                "largeSequenceSize": 100,
+                "displayLinksArrow": true,
+                "type": "mindmap",
+                "label": "Mindmap",
+                "mediumSequenceSize": 60
+            }
+        ]
+    },
+    "mainSequence": "0AB7F235-2012-2206-6968-7CC7582C3BF4",
+    "wording": {
+        "program_percentage_viewed": "du programme vu",
+        "total_sequences": "séquences au total",
+        "your_are_here": "Vous êtes ici",
+        "keywords": "mots-clés",
+        "total_duration": "(durée totale : {0})"
+    },
+    "general": {
+        "editorVersionLabel": "Klynt 3.5.007",
+        "overlayCloseButtonRight": 16,
+        "mapsKey": "",
+        "mainFontName": "Open Sans",
+        "projectId": "E6749A87-96EB-B44E-71D3-7CC757CD35B3",
+        "creationDate": 1745850161101,
+        "mainFontSize": 14,
+        "width": 1920,
+        "level": 5,
+        "keywords": null,
+        "ratio": "16/9",
+        "height": 1080,
+        "modificationDate": 1747051665325,
+        "author": "Ekari",
+        "title": "TEST",
+        "primaryColor": 3355443,
+        "username": "piney9999",
+        "projectHost": "",
+        "secondaryColor": 16777215,
+        "description": "",
+        "editorVersionNumber": "3.5.007",
+        "overlayCloseButtonTop": 16,
+        "analyticsKey": "",
+        "tertiaryColor": 15286861
+    },
+    "advanced": {
+        "enableFullWindowMode": true,
+        "enableKeyboardNavigation": true,
+        "enableScrollNavigation": true,
+        "hasRemoteVideos": false,
+        "remoteVideosAPILocalBitrate": "best",
+        "enableSwipeNavigation": true,
+        "remoteVideosAPIURL": "http://api.klynt.net/video/klynt-video-api.php"
+    },
+    "watermark": {
+        "action": {
+            "type": "openSequence",
+            "target": "null"
+        },
+        "image": "Player/css/editor/img/watermark.png",
+        "displayOnStartSequence": true,
+        "enabled": false,
+        "position": "top-left"
+    },
+    "aliases": {
+        "idToAlias": {
+            "0C7A1342-B5C6-03CD-3278-7CC9FB61D9AA": "G_MAN",
+            "27D0F491-3F41-F243-3294-7CCCD08F6FF2": "G_MAN_VID",
+            "0AB7F235-2012-2206-6968-7CC7582C3BF4": "Home"
+        },
+        "aliasToId": {
+            "Home": "0AB7F235-2012-2206-6968-7CC7582C3BF4",
+            "G_MAN": "0C7A1342-B5C6-03CD-3278-7CC9FB61D9AA",
+            "G_MAN_VID": "27D0F491-3F41-F243-3294-7CCCD08F6FF2"
+        }
+    },
+    "footer": {
+        "height": 26,
+        "items": [
+            {
+                "action": {
+                    "type": "openWidget",
+                    "target": "E01AC54F-0FCA-A95E-1B15-7CC75816804F"
+                },
+                "label": "Index"
+            },
+            {
+                "action": {
+                    "type": "openWidget",
+                    "target": "4AD34FA5-BF3D-59A4-DFBF-7CC7581484E2"
+                },
+                "label": "Credits"
+            }
+        ],
+        "logo": {
+            "image": null,
+            "url": "http://www.klynt.net",
+            "tooltip": "null"
+        },
+        "buttons": [
+            {
+                "icon": "share",
+                "type": "share",
+                "label": "Share"
+            },
+            {
+                "icon": "sound",
+                "type": "sound",
+                "label": "Sound"
+            },
+            {
+                "icon": "fullscreen",
+                "type": "fullscreen",
+                "label": "Fullscreen"
+            },
+            {
+                "icon": "klynt",
+                "type": "klynt",
+                "label": "Made with"
+            }
+        ]
+    },
+    "sequences": {
+        "0C7A1342-B5C6-03CD-3278-7CC9FB61D9AA": {
+            "id": "0C7A1342-B5C6-03CD-3278-7CC9FB61D9AA",
+            "title": "G MAN",
+            "hideInMindmap": false,
+            "y": 200.4,
+            "duration": 30,
+            "x": 949.8333333333334,
+            "buttons": [
+                {
+                    "id": "2F18A86F-E508-3ED1-3F2D-7CCC94BC8140",
+                    "dataend": "00:31.00",
+                    "duration": "00:31.00",
+                    "zIndex": 1,
+                    "centerX": "0.00",
+                    "width": "160.00",
+                    "label": "Home",
+                    "height": "40.00",
+                    "scales": false,
+                    "centerY": "150.00",
+                    "link": {
+                        "controlDY": 0,
+                        "id": "D48BA9E9-16A7-3F8F-6D6B-7CCC94BF0366",
+                        "targetSequence": "0AB7F235-2012-2206-6968-7CC7582C3BF4",
+                        "controlDX": 0,
+                        "automaticTransition": false,
+                        "linkType": "linkToSequence",
+                        "transition": "fade",
+                        "hideInMindmap": false,
+                        "hideArrowInMindmap": false,
+                        "color": "rgba(0,0,0,1)"
+                    },
+                    "type": "klynt-button-1",
+                    "databegin": "00:00.00",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    }
+                },
+                {
+                    "id": "DB1B865E-9084-9F7F-E1C9-7CCD1D73E9FF",
+                    "dataend": "00:31.00",
+                    "duration": "00:31.00",
+                    "zIndex": 2,
+                    "centerX": "166.99",
+                    "width": "160.00",
+                    "label": "G MAN VID",
+                    "height": "40.00",
+                    "scales": false,
+                    "centerY": "151.00",
+                    "link": {
+                        "id": "7A83339E-549A-9565-AE79-7CCD1D7574B1",
+                        "controlDX": 0,
+                        "hideInMindmap": false,
+                        "controlDY": 0,
+                        "pauseParent": true,
+                        "closeButtonRight": 16,
+                        "closeButton": true,
+                        "hideArrowInMindmap": false,
+                        "linkType": "linkToSequence",
+                        "targetSequence": "27D0F491-3F41-F243-3294-7CCCD08F6FF2",
+                        "automaticTransition": false,
+                        "closeButtonTop": 16,
+                        "overlay": true,
+                        "automaticClose": false,
+                        "transition": "fade",
+                        "color": "rgba(0,0,0,1)"
+                    },
+                    "type": "klynt-button-1",
+                    "databegin": "00:00.00",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    }
+                }
+            ],
+            "backgroundColor": "rgba(0,0,0,0.75)",
+            "hideTitleInMindmap": false,
+            "hideThumbnailInMindmap": false,
+            "size": "small",
+            "color": "rgba(0,0,0,1)"
+        },
+        "27D0F491-3F41-F243-3294-7CCCD08F6FF2": {
+            "videos": [
+                {
+                    "id": "F058A7D5-7B67-685A-15A8-7CDA17F7F8BC",
+                    "src": [
+                        {
+                            "type": "video/mp4",
+                            "src": "Medias/Videos/example.mp4"
+                        }
+                    ],
+                    "dataend": "00:02.37",
+                    "syncMaster": true,
+                    "verticalAlign": "middle",
+                    "frameRate": 0,
+                    "zIndex": 1,
+                    "mediaHeight": 360,
+                    "name": "example.mp4",
+                    "mediaWidth": 640,
+                    "fitToWindow": true,
+                    "duration": "00:02.37",
+                    "loop": true,
+                    "autoplay": true,
+                    "scaleMode": "zoom",
+                    "poster": "Medias/Posters/example.mp4.jpg",
+                    "horizontalAlign": "center",
+                    "player": false,
+                    "volume": 1,
+                    "databegin": "00:00.00",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    }
+                }
+            ],
+            "id": "27D0F491-3F41-F243-3294-7CCCD08F6FF2",
+            "title": "G MAN VID",
+            "hideInMindmap": false,
+            "y": 414.85,
+            "duration": 2.368,
+            "x": 973.6833333333334,
+            "backgroundColor": "rgba(0,0,0,0.75)",
+            "hideTitleInMindmap": false,
+            "thumbnail": "Medias/Thumbnails/example.mp4.jpg",
+            "hideThumbnailInMindmap": false,
+            "size": "small",
+            "color": "rgba(0,0,0,1)"
+        },
+        "0AB7F235-2012-2206-6968-7CC7582C3BF4": {
+            "id": "0AB7F235-2012-2206-6968-7CC7582C3BF4",
+            "title": "Home",
+            "hideInMindmap": false,
+            "y": 279.15000000000003,
+            "duration": 30,
+            "x": 645.4833333333333,
+            "buttons": [
+                {
+                    "id": "BC65090F-2B20-DA8E-7C72-7CCA47384BC4",
+                    "dataend": "00:31.00",
+                    "duration": "00:31.00",
+                    "zIndex": 2,
+                    "centerX": "-17.00",
+                    "width": "160.00",
+                    "label": "G MAN",
+                    "height": "40.00",
+                    "scales": false,
+                    "centerY": "198.00",
+                    "link": {
+                        "controlDY": 0,
+                        "id": "4D48B0FF-F77A-B633-2086-7CCA473E9787",
+                        "targetSequence": "0C7A1342-B5C6-03CD-3278-7CC9FB61D9AA",
+                        "controlDX": 0,
+                        "automaticTransition": false,
+                        "linkType": "linkToSequence",
+                        "transition": "fade",
+                        "hideInMindmap": false,
+                        "hideArrowInMindmap": false,
+                        "color": "rgba(0,0,0,1)"
+                    },
+                    "type": "klynt-button-1",
+                    "databegin": "00:00.00",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    }
+                },
+                {
+                    "id": "5AB0D534-64C2-B3E6-A34F-7CCC5E445AFF",
+                    "dataend": "00:31.00",
+                    "duration": "00:31.00",
+                    "zIndex": 3,
+                    "centerX": "140.00",
+                    "width": "100.00",
+                    "label": "G MAN",
+                    "height": "100.00",
+                    "scales": false,
+                    "centerY": "-170.00",
+                    "link": {
+                        "controlDY": 0,
+                        "id": "64B53C23-140D-01A3-C18B-7CCC5E459918",
+                        "targetSequence": "0C7A1342-B5C6-03CD-3278-7CC9FB61D9AA",
+                        "controlDX": 0,
+                        "automaticTransition": false,
+                        "linkType": "linkToSequence",
+                        "transition": "fade",
+                        "hideInMindmap": false,
+                        "hideArrowInMindmap": false,
+                        "color": "rgba(0,0,0,1)"
+                    },
+                    "type": "klynt-button-1",
+                    "databegin": "00:00.00",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    }
+                },
+                {
+                    "id": "815B0740-07BE-BA15-8B36-7CCC694F8E94",
+                    "dataend": "00:31.00",
+                    "duration": "00:31.00",
+                    "zIndex": 4,
+                    "centerX": "-313.00",
+                    "width": "100.00",
+                    "label": "G MAN",
+                    "height": "100.00",
+                    "scales": false,
+                    "centerY": "115.00",
+                    "link": {
+                        "controlDY": 0,
+                        "id": "8A8F46C8-30D7-C18E-DF6C-7CCC695163F0",
+                        "targetSequence": "0C7A1342-B5C6-03CD-3278-7CC9FB61D9AA",
+                        "controlDX": 0,
+                        "automaticTransition": false,
+                        "linkType": "linkToSequence",
+                        "transition": "fade",
+                        "hideInMindmap": false,
+                        "hideArrowInMindmap": false,
+                        "color": "rgba(0,0,0,1)"
+                    },
+                    "type": "klynt-button-1",
+                    "databegin": "00:00.00",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    }
+                },
+                {
+                    "id": "CBFCD3EE-52E8-12F8-B642-7CCC79E9E1BC",
+                    "dataend": "00:31.00",
+                    "duration": "00:31.00",
+                    "zIndex": 5,
+                    "centerX": "228.00",
+                    "width": "100.00",
+                    "label": "G MAN",
+                    "height": "100.00",
+                    "scales": false,
+                    "centerY": "210.00",
+                    "link": {
+                        "controlDY": 0,
+                        "id": "76374277-D255-B28B-87DC-7CCC79EB514B",
+                        "targetSequence": "0C7A1342-B5C6-03CD-3278-7CC9FB61D9AA",
+                        "controlDX": 0,
+                        "automaticTransition": false,
+                        "linkType": "linkToSequence",
+                        "transition": "fade",
+                        "hideInMindmap": false,
+                        "hideArrowInMindmap": false,
+                        "color": "rgba(0,0,0,1)"
+                    },
+                    "type": "klynt-button-1",
+                    "databegin": "00:00.00",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    }
+                }
+            ],
+            "backgroundColor": "rgba(0,0,0,1)",
+            "hideTitleInMindmap": false,
+            "thumbnail": "Medias/Thumbnails/IMG_2393.jpeg.jpg",
+            "hideThumbnailInMindmap": false,
+            "size": "large",
+            "images": [
+                {
+                    "id": "4FE7F8AE-8520-8BA2-4B52-7CC7CFB46E3C",
+                    "src": "Medias/Photos/IMG_2393.jpeg",
+                    "dataend": "00:31.00",
+                    "mediaWidth": 400,
+                    "verticalAlign": "middle",
+                    "duration": "00:31.00",
+                    "zIndex": 1,
+                    "mediaHeight": 400,
+                    "name": "IMG_2393.jpeg",
+                    "fitToWindow": true,
+                    "scaleMode": "zoom",
+                    "horizontalAlign": "center",
+                    "databegin": "00:00.00",
+                    "transitionIn": {
+                        "type": "fade",
+                        "duration": "1"
+                    }
+                }
+            ],
+            "color": "rgba(0,0,0,1)"
+        }
+    },
+    "share": {
+        "link": null,
+        "socialNetworksWording": "Social Networks",
+        "displayMiniPlayerWording": true,
+        "thumbnail": null,
+        "enableGooglePlus": true,
+        "title": "",
+        "enableLinkedIn": true,
+        "originalSizeWording": "Original",
+        "embedWording": "Embed",
+        "enableTumblr": true,
+        "verticalWording": "Vertical",
+        "shortMessage": "",
+        "message": "",
+        "playerWording": "Player",
+        "removeViaKlynt": true,
+        "horizontalWording": "Horizontal",
+        "customSizeWording": "Custom",
+        "miniPlayerWording": "Mini player",
+        "shareSequenceWording": "Share sequence",
+        "linkWording": "Link"
+    }
+}
---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "resumePlaybackWording": "Resume playback?",
+    "redirectToMobileApp": "auto",
+    "noWording": "No",
+    "analyticsKey": "",
+    "launchAppWording": "Then Launch Project",
+    "title": "TEST",
+    "yesWording": "Yes",
+    "downloadAppWording": "Download App",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "url": "",
+    "description": "",
+    "thumbnail": "Medias/Photos/IMG_2393.jpeg"
+}
